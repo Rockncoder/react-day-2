7d43263117990569b7c18d21d919f64bd5580738
Troy Miles <rockncoder@gmail.com>
2018-03-18 10:48:42 -0700

    Semi-functional

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuidv4 from 'uuid/v4';
 import AddTodo from './AddTodo';
+import Todo from './Todo';
 import {toggleDone, addTodo, deleteTodo} from './state-functions';
 
 export default class TodoList extends React.Component {
@@ -19,12 +20,26 @@ export default class TodoList extends React.Component {
     this.setState(addTodo(this.state, todo));
   };
 
+  renderTodos(){
+    return this.state.todos.map(todo => (
+      <li key={todo.id}>
+        <Todo
+          todo={todo}
+          doneChange={id => this.toggleDone(id)}
+          deleteTodo={id => this.deleteTodo(id)}
+        />
+      </li>
+    ));
+  }
 
   render() {
     return (
       <div>
         <AddTodo onNewTodo={todo => this.addTodo(todo)} />
         <h2>Todos:</h2>
+        <ul className="todos-list">
+          {this.renderTodos()}
+        </ul>
       </div>
     )
   }
