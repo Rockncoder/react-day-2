43cf5386c784d4e09374c551730746bee82c87a3
Troy Miles <rockncoder@gmail.com>
2018-03-18 09:14:54 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import TodoList from './TodoList';
+
+render(
+  <TodoList />,
+  document.getElementById('root')
+);
