74f8cf732ba8f52440daf4c4335aa2b5981dfa23
Troy Miles <rockncoder@gmail.com>
2018-03-18 16:27:55 -0700

    Working version of calc + redux devtools hook

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducer from './reducer';
 
-const store = createStore(reducer);
+// const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
 
 render(
   <Provider store={store}>
